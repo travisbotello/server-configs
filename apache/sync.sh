65d2c6d8292788fe79ee63e7c510e4507f8cec57
AD7six <andydawson76@gmail.com>
2012-04-03 10:44:45 +0200

    adding a script to sync .htaccess file
    looks for 2 markers in sites-availble/default - and injects the contents
    of the .htaccess file from h5bp's main repo

---
+++ apache/sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Updates sites-available/default with the htaccess file from h5bp's main repo
+
+cd /tmp
+rm .htaccess
+wget --no-check-certificate https://raw.github.com/h5bp/html5-boilerplate/master/.htaccess
+
+cd -
+sed  's/^/                /'  /tmp/.htaccess > .htaccess
+
+awk -v htaccess=.htaccess '
+	/### .htaccess file contents ###/ {print; system("cat " htaccess); step=1; next}
+	/### END .htaccess file contents ###/ {step=0}
+	step {next}
+	{print}
+' sites-available/default > /tmp/h5bp-default && mv /tmp/h5bp-default sites-available/default
